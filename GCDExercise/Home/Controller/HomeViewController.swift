fb34a200da35b813b65a21a8adecbfc96c36b85e
PhanNguyen98 <nguyen070498@gmail.com>
2020-12-20 20:45:57 +0700

    GCDExercise

---
+++ GCDExercise/Home/Controller/HomeViewController.swift
@@ -0,0 +1,59 @@
+//
+//  HomeViewController.swift
+//  GCDExercise
+//
+//  Created by Phan Nguyen on 17/12/2020.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    
+    let data = ["https://www.wcrf-uk.org/sites/default/files/Apple_A-Z%20Fruit1.jpg",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyMJT8U6qsRUTjujjRbtvWaBDaWb117ez43Q&usqp=CAU",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT67evcrI2WUip8dtI0FP7awtxE6eQpXuHajg&usqp=CAU",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf40dSsgHV969kuWZhRZr5IqJuHopNZvSk6w&usqp=CAU",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrgQl7av2s2J_QFMxtjIQnrZXdF_GmLX6nZw&usqp=CAU",]
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    let queue = DispatchQueue(label: "Serial")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTableView()
+    }
+    
+    func setTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "HomeTableViewCell", bundle: nil), forCellReuseIdentifier: "HomeTableViewCell")
+    }
+}
+
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell", for: indexPath) as? HomeTableViewCell else {
+            return HomeTableViewCell()
+        }
+        //task2
+        cell.tag = indexPath.row
+        cell.setImageFromUrl(ImageURL: data[indexPath.row % 5], index: indexPath.row)
+        
+        //task1
+//        cell.tag = indexPath.row
+//        cell.setImageFromUrl(ImageURL: data[indexPath.row % 5], index: indexPath.row)
+        return cell
+    }
+}
---
+++ GCDExercise/Home/View/HomeTableViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  TableViewCell.swift
+//  GCDExercise
+//
+//  Created by Phan Nguyen on 17/12/2020.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var fruitImageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        fruitImageView.image = nil
+        if let activityIndicator = self.activityIndicator {
+            activityIndicator.startAnimating()
+        }
+    }
+    
+    func setImageFromUrl(ImageURL :String, index: Int) {
+        let queue = DispatchQueue(label: "Concurrent",qos: .background , attributes: .concurrent)
+        queue.async {
+            let task = URLSession.shared.dataTask( with: NSURL(string:ImageURL)! as URL, completionHandler: { (data, response, error) -> Void in
+                DispatchQueue.main.async {
+                    if self.tag == index {
+                        if let data = data {
+                            self.fruitImageView.image = UIImage(data: data)
+                            self.activityIndicator.stopAnimating()
+                        }
+                    }
+                }
+            })
+            task.resume()
+        }
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
